45e0ed82971ff3b41fcf2b3ea8eea56f7fcec8b2
BOEHKOMAT <44070912+BOEHKOMAT@users.noreply.github.com>
2018-10-17 17:25:08 +0300

    Update alglab11.c

--- lab1/alglab11.c
+++ lab1/alglab11.c
@@ -5,7 +5,10 @@ int main(void)
 {
     int a = 1000;
     float b = 0.0001;
-    double c;
-    c = (pow(a - b,3)) - (pow(a,3) - 3 * a * pow(b,2)) / (pow(b,3) - 3 * pow(a,2) * b);
-    printf("%f\n",c);
+    double c1;
+    float c2;
+    c1 = (pow(a - b,3)) - (pow(a,3) - 3 * a * pow(b,2)) / (pow(b,3) - 3 * pow(a,2) * b);
+    c2 = (pow(a - b,3)) - (pow(a,3) - 3 * a * pow(b,2)) / (pow(b,3) - 3 * pow(a,2) * b);
+    printf("Result(type double):%f\n",c1);
+    printf("Result(type float):%f\n",c2);
 }
