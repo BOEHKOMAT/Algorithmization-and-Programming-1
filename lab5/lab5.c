f68e8a0a397c7ec17a5a841d1d2b2c8c571cd9e2
BOEHKOMAT <44070912+BOEHKOMAT@users.noreply.github.com>
2018-11-12 20:27:25 +0200

    Update lab5.c

--- lab5/lab5.c
+++ lab5/lab5.c
@@ -1,5 +1,88 @@
 #include<stdio.h>
 #include<cs50.h>
+#include<time.h>
+void printmat(int A[100][100], int *a, int *b)
+{
+    for(int i = 0 ; i < *a ; i++)
+    {
+        for(int j = 0 ; j < *b ; j++)
+        {
+            A[i][j] = rand()%90+10;
+            printf("%i ", A[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void check_max_min(int A[100][100], int r, int c)
+{
+    int max[r];
+    int index = 0;
+    int check;
+    for(int i = 0 ; i < r ; i++)
+    {
+        max[i] = A[i][0];
+        index = 0;
+        for(int j = 0 ; j < c ; j++)
+        {
+            if(A[i][j] > max[i])
+                {
+                    max[i] = A[i][j];
+                    index = j;
+                }
+        }
+        //printf("|%i|", max[i]);
+        //printf("|%i|", index);
+        check = 1;
+        for(int k = 0; k < r ; k++)
+        {
+            if(max[i] < A[k][index])
+                {
+                    check++;
+                    //printf("%i", check);
+                }
+                
+        } 
+        //printf("\t");
+        if(check == r)
+            printf("{%i}", max[i]);
+    }
+}
+
+void check_min_max(int A[100][100], int r, int c)
+{
+    int max[c];
+    int index = 0;
+    int check;
+    for(int i = 0 ; i < c ; i++)
+    {
+        max[i] = A[0][i];
+        index = 0;
+        for(int j = 0 ; j < r ; j++)
+        {
+            if(A[j][i] > max[i])
+                {
+                    max[i] = A[j][i];
+                    index = j;
+                }
+        }
+        //printf("|%i|", max[i]);
+        //printf("|%i|", index);
+        check = 1;
+        for(int k = 0; k < c ; k++)
+        {
+            if(max[i] < A[index][k])
+                {
+                    check++;
+                    //printf("%i", check);
+                }
+                
+        } 
+        //printf("\t");
+        if(check == c)
+            printf("{%i}", max[i]);
+    }
+}
 
 int main(void)
 {
@@ -7,120 +90,14 @@ int main(void)
     printf("Enter count of raws:");
     scanf("%i",&a);
     int b;
-    printf("Enter count of colums:\n");
+    printf("Enter count of colums:");
     scanf("%i",&b);
-    int arr[a][b], max[2][a], min[2][b], maxi[2][b], mini[2][a];
-    int c, d;
-    for(c = 0 ; c < a ; c++)
-    {
-        for(d = 0 ; d < b ; d++)
-        {
-            arr[c][d]=rand()%9000+1000;
-            printf("|%i|", arr[c][d]);
-        }
-        printf("\n");
-    }
+    int arr[a][b];
+    srand(time(NULL));
+    
+    printmat(arr,&a,&b);
+    printf("Sadle point(s): ");
+    check_max_min(arr,a,b);
+    check_min_max(arr,a,b);
     printf("\n");
-    for(c = 0 ; c < a ; c++)
-    {
-        max[0][c] = arr[c][0];
-        max[1][c] = 0;
-        for(d = 0 ; d < b ; d++)
-        {
-            if(arr[c][d] > max[0][c])
-                {
-                    max[0][c] = arr[c][d];  //max in raws
-                    max[1][c] = c;
-                }
-        }
-    }
-    for(d = 0 ; d < b ; d++)
-    {
-        min[0][d] = arr[0][d];
-        min[1][d] = 0;
-        for(c = 0 ; c < a ; c++)
-        {
-            if(arr[c][d] < min[0][d])
-                {
-                    min[0][d] = arr[c][d];  //min in columns
-                    min[1][d] = c;
-                }
-        }
-    }
-    
-    
-    for(c = 0 ; c < b  ; c++)
-    {
-        for(d = 0 ; d < b ; d++)
-        {
-            if((max[0][c] == min[0][d]) && (max[1][c] == min[1][c]))    //find out a sadle point
-                {
-                    printf("\n|%i|\n", max[0][c]);
-                }
-        }
-    }
-    
-   /* for(c = 0 ; c < a ; c++)
-        {
-            printf("|%i|", max[0][c]);
-            printf("|%i|", max[1][c]);
-        }
-        printf("\n");
-    for(d = 0 ; d < b ; d++)
-        {
-            printf("|%i|", min[0][d]);
-            printf("|%i|", min[1][d]);
-        }
-        printf("\n");*/
-    
-    for(d = 0 ; d < b ; d++)
-    {
-        maxi[0][d] = arr[0][d];
-        maxi[1][d] = 0;
-        for(c = 0 ; c < a ; c++)
-        {
-            if(arr[c][d] > maxi[0][d])
-                {
-                    maxi[0][d] = arr[c][d];  //max in columns
-                    maxi[1][d] = d;
-                }
-        }
-    }
-    for(c = 0 ; c < a ; c++)
-    {
-        mini[0][c] = arr[c][0];
-        mini[1][c] = 0;
-        for(d = 0 ; d < b ; d++)
-        {
-            if(arr[c][d] < mini[0][c])
-                {
-                    mini[0][c] = arr[c][d];  //min in raws
-                    mini[1][c] = d;
-                }
-        }
-    }
-    
-   /* for(c = 0 ; c < a ; c++)
-        {
-            printf("|%i|", mini[0][c]);
-            printf("|%i|", mini[1][c]);
-        }
-        printf("\n");
-    for(d = 0 ; d < b ; d++)
-        {
-            printf("|%i|", maxi[0][d]);
-            printf("|%i|", maxi[1][d]);
-        }
-        printf("\n");*/
-    
-    for(d = 0 ; d < b  ; d++)
-    {
-        for(c = 0 ; c < a ; c++)
-        {
-            if((maxi[0][d] == mini[0][c]) && (maxi[1][d] == mini[1][c]))    //find out a sadle point
-                {
-                    printf("\n|%i|\n", maxi[0][d]);
-                }
-        }
-    }
 }
