7a1fc4283643ebdfd76e076362edd11f96efb688
BOEHKOMAT <44070912+BOEHKOMAT@users.noreply.github.com>
2018-11-12 19:42:15 +0200

    Update alglab4.c

--- lab4/alglab4.c
+++ lab4/alglab4.c
@@ -1,100 +1,79 @@
 #include<stdio.h>
 #include<cs50.h>
+#include<time.h>
+void pmas(int E[], int e)
+{
+    for(int i = 0; i < e; i++)
+    {
+        printf("|%i|", E[i]);
+    }
+    printf("\n");
+}
+
+void del(int E[], int e, int d)
+{
+    for(int i = 0; i < e; i++)
+    {
+        if(i >= d)
+            {
+                E[i] = E[i+1];
+            }
+    }
+}
+
+void equal(int A[], int B[], int s)
+{
+    for(int i = 0 ; i < s ; i++)
+    {
+        B[i] = A[i];
+    }
+    printf("\n");
+}
 
 int main(void)
 {
+    srand(time(NULL));
     int x, x1, i;
-    printf("Enter x:\n");
+    printf("Choose size of array: ");
     scanf("%i", &x);
     int M[x];
-    int N[x];
-    int a = 0;
+    int N[x-1];
     
-    for(i = 0;i<x;i++)
+    for(i = 0; i < x; i++)
         {
-            M[i]=N[i];
-            M[i] = rand()%100;
-            printf("|%i|", M[i]);
+            M[i] = rand()%20;
         }
+    pmas(M,x);
     
-    printf("\n");
-    printf("Choose array element to remove it:\n");
+    printf("Choose array element to remove it: ");
     scanf("%i",&x1);
     
-    M[x1] = 0;
+    del(M,x,x1);
+    equal(M,N,x-1);
+    pmas(M,x-1);
     
-    for(i = 0;i<x;i++)
+    printf("|%i|", M[0]);
+    for(i = 1 ; i < x ; i++)
     {
-        if(x1 != i)
-            printf("|%i|", M[i]);
-        else
-            continue;
-    }
-    printf("\n");
-    
-    i = 0;
-    printf("Array with added element followed after the first paired element:\n");
-    do
-    {   
-        if(M[i]%2!=0)
+        if(M[i] == 0)
+            {
+                continue;
+            }
+        else if(M[i] % 2 != 0)
             {
                 printf("|%i|", M[i]);
-                i++;
-            }
-        else if(M[i]%2==0&&x1==i)
-            {
-                a++;
-                i++;
-                break;
             }
         else
-            { 
-                N[i+1]=M[i-1]+2;
+            {
                 printf("|%i|", M[i]);
-                printf("|%i|", N[i+1]);
-                i++;
+                M[i+1] = M[i-1] + 2;
+                printf("|%i|", M[i+1]);
+                for(int j = i + 1 ; j < x - 1 ; j++)
+                {
+                    printf("|%i|", N[j]);
+                }
                 break;
             }
     }
-    while(true);
-    do
-    {   
-        if(a == 0)
-            {
-                if(x1 != i)
-                {
-                    printf("|%i|", M[i]);
-                    i++;
-                }
-                else
-                {
-                    i++;
-                    continue;
-                }
-            }
-        else
-            {
-                if(M[i]%2!=0)
-                    {
-                        printf("|%i|", M[i]);
-                        i++;
-                    }
-                else
-                    { 
-                        N[i+1]=M[i-1]+2;
-                        printf("|%i|", M[i]);
-                        printf("|%i|", N[i+1]);
-                        i++;
-                        do
-                        {
-                            printf("|%i|", M[i]);
-                            i++;
-                        }
-                        while(i < x);
-                        break;
-                    }
-            }
-    }
-    while(i < x);
     printf("\n");
 }
